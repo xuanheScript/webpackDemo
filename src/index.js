c5221fa21634f0026051c69e89ae05accb764b5e
李杰 <lj706517903@163.com>
2018-04-26 15:50:47 +0800

    First

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import store from './store';
+
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+moment.locale('zh-cn');
+
+
+if(!window.Promise) {
+  document.writeln('<script src="https://as.alipayobjects.com/g/component/es6-promise/3.2.2/es6-promise.min.js></script>"');
+}
+
+
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
+registerServiceWorker();
